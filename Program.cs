9bd6e7d68b133543e6d4399221546b55ef9f67ff
4view <akochergin.2002@gmail.com>
2024-06-13 15:34:38 +0500

    Fitst commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+       
+    }   
+    
+    
+}
