5035509367339574ee1fb9f8bcfd459330123944
4view <akochergin.2002@gmail.com>
2024-06-14 16:54:23 +0500

    working with abstracr class

--- Program.cs
+++ Program.cs
@@ -8,8 +8,38 @@ class Program
 {
     static void Main(string[] args)
     {   
-       
+       Car bmw = new Car("BMW");
+
+       bmw.Move();
     }   
     
     
 }
+
+abstract class Transport
+{
+
+    public string Name { get; }
+
+    public Transport(string name)
+    {
+        Name = name;
+    }
+    public void Move() => Console.WriteLine($"{Name} is moving");
+}
+
+class Ship : Transport 
+{
+    public Ship(string name) : base(name) {}
+}
+
+class Aircraft : Transport 
+{
+    public Aircraft(string name) : base(name) {}
+}
+
+class Car : Transport 
+{
+    public Car(string name) : base(name) {}
+}
+
